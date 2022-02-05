6ada9df93ef30cdf0210c6c8b89e280aae0b1b04
James Carlson <jxxcarlson@gmail.com>
2022-02-05 12:02:38 -0500

    refactor

--- brownian_motion/main.ml
+++ brownian_motion/main.ml
@@ -15,20 +15,20 @@ open Point
 
 let step_size = 10;;
 let color_step_size = 6;;
-
 let number_of_steps = 4000;;
 let number_of_steps_in_cloud = 200;;
 let points_in_cloud = 3000;;
-let window_width = 800;;
-let window_height = 800;;
+
 
 (* PROGRAM *)
+
+let window_width = 800;;
+let window_height = 800;;
 
 (* Compute Brownian path *)
 let origin = {x = window_width/2; y = window_height/2; radius = 2; r = 125; g = 0; b = 125};;
 let center = {x = window_width/2; y = window_height/2; radius = 10; r = 160; g = 160; b = 210};;
 let path = Brownian.path origin step_size color_step_size number_of_steps;;
 let cloud = Brownian.cloud origin step_size color_step_size number_of_steps_in_cloud points_in_cloud;;
-let data = center :: List.append cloud path;;
 
-Canvas.run (origin :: data) window_width window_height
+Canvas.run (origin :: (List.append cloud path)) window_width window_height
