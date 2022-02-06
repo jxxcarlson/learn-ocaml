ca876d5854ecab3b4e3108f82128d44581791338
James Carlson <jxxcarlson@gmail.com>
2022-02-06 16:43:23 -0500

    add type initialData

---
+++ life/canvas.mli
@@ -0,0 +1,12 @@
+
+
+(* Display point list, e.g.,
+
+   run points width height
+
+where width and height are the dimesions of the
+window in pixels.
+
+ *)
+
+val run: Point.point list -> int -> int -> unit
